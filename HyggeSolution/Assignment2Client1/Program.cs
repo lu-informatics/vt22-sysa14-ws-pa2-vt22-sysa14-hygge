3693440a6b3f53674116dcb3520ca54a7106d1d1
karinengdal <karinlisa.engdal@gmail.com>
2022-02-18 10:18:05 +0000

    fixed

---
+++ HyggeSolution/Assignment2Client1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment2Client1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
