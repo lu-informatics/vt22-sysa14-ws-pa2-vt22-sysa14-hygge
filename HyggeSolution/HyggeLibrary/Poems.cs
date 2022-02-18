065d306a1eaf3dc203cd73b988d388fbfa86ee30
karinengdal <karinlisa.engdal@gmail.com>
2022-02-18 12:48:10 +0000

    properties

--- HyggeSolution/HyggeLibrary/Poems.cs
+++ HyggeSolution/HyggeLibrary/Poems.cs
@@ -11,5 +11,9 @@ namespace HyggeLibrary
         private string poemID; 
         private string poemName;
         private string poemContent;
+
+        public string PoemID { get => poemID; set => poemID = value; }
+        public string PoemName { get => poemName; set => poemName = value; }
+        public string PoemContent { get => poemContent; set => poemContent = value; }
     }
 }
