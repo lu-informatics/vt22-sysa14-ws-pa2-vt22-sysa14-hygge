98c41479a3b7768c73f0f869cecea7d26391ac9c
karinengdal <karinlisa.engdal@gmail.com>
2022-02-18 12:46:01 +0000

    Created Poems in "HyggeLibrary"

---
+++ HyggeSolution/HyggeLibrary/Poems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyggeLibrary
+{
+    public class Poems
+    {
+        private string poemID; 
+        private string poemName;
+        private string poemContent;
+    }
+}
