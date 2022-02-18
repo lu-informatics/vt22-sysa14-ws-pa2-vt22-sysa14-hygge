a048a98ac664d2db70d362c791e9c828dd9224f2
karinengdal <karinlisa.engdal@gmail.com>
2022-02-18 13:23:22 +0000

    changes

--- HyggeSolution/HyggeLibrary/Poems.cs
+++ HyggeSolution/HyggeLibrary/Poems.cs
@@ -12,6 +12,13 @@ namespace HyggeLibrary
         private string poemName;
         private string poemContent;
 
+        public Poems(string poemID, string poemName, string poemContent)
+        {
+            this.poemID = poemID;
+            this.poemName = poemName;
+            this.poemContent = poemContent;
+        }
+
         public string PoemID { get => poemID; set => poemID = value; }
         public string PoemName { get => poemName; set => poemName = value; }
         public string PoemContent { get => poemContent; set => poemContent = value; }
--- HyggeSolution/HyggeWS/HyggeService.asmx.cs
+++ HyggeSolution/HyggeWS/HyggeService.asmx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HyggeLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,9 @@ namespace HyggeWS
         {
             return "Hello World";
         }
-     
+
+        }
+
+
     }
-}
+
