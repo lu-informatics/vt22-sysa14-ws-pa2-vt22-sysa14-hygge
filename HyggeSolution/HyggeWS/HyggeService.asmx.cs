ee03eb5700f03cdd7ca31488635b12b85e4b089c
karinengdal <karinlisa.engdal@gmail.com>
2022-02-21 07:27:48 +0000

    fix poem web service with web method
    Added a web method that takes a file name as input and outputs file content, according to the assignment.

--- HyggeSolution/HyggeWS/HyggeService.asmx.cs
+++ HyggeSolution/HyggeWS/HyggeService.asmx.cs
@@ -1,5 +1,6 @@
 ﻿using HyggeLibrary;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,29 +18,29 @@ namespace HyggeWS
     // [System.Web.Script.Services.ScriptService]
     public class HyggeService : System.Web.Services.WebService
     {
-
         [WebMethod]
-        public string HelloWorld()
+        public string FetchPoem(string fileName) //Method to fetch file content using fileName from user (Eclipse --> WS)
         {
-            return "Hello World";
+            if (fileName == null)                //Null check
+            {
+                return "Error: File was not found";
+            }
+
+            string path = @"C:\poems\" + fileName;  //Creating a path string to be used later to find the correct file
+
+            try
+            {
+                string text = File.ReadAllText(path);   //Put content of a file in string using the path created above
+                return text;        //Return the file content
+            }
+            catch (FileNotFoundException ex)    //Catching FileNotFoundException in case File.ReadAllText throws it
+            {
+                return "Error: No file with file name " + fileName; //If the file was not found, return error message
+            }
         }
-
-        [WebMethod]
-        public string ShowFileOne()
-        {
-            return "This is file one!";
-        }
-        [WebMethod]
-        public string ShowFileTwo()
-        {
-            return "This is file one!";
-        }
-
-
-
-        }
-
-
-
     }
+
+
+
+}
 
